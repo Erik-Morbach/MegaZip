0b3d10f6250685659cad6f713857c904da21d4e6
Erik Morbach <morbacherik@gmail.com>
2025-11-19 20:12:40 -0300

    Adding structure

---
+++ main.cpp
@@ -0,0 +1,42 @@
+namespace Compress {
+	void read(){
+
+	}
+
+	void compress(){
+
+	}
+
+	void write(){
+
+	}
+};
+
+namespace Decompress {
+	void decompress(){
+
+	}
+};
+
+namespace Monitor {
+	void main(){
+		setup();
+		while(1){
+			update();
+		}
+		finish();
+	}
+	void setup(){
+
+	}
+	void update(){
+
+	}
+	void finish(){
+
+	}
+};
+
+int main(){
+	return 0;
+}
